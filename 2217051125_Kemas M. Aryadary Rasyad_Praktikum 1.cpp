a6d88190beaa0b8032cc51ac48c18ea8b102ed64
aryadary0706 <112590882+aryadary0706@users.noreply.github.com>
2022-09-09 12:49:17 +0700

    Add files via upload

---
+++ 2217051125_Kemas M. Aryadary Rasyad_Praktikum 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string x,y,z,p;
+	cout<<"nama : ";
+	getline(cin,x);
+	cout<<"NPM : ";
+	getline(cin,y);
+	cout<<"Alamat : ";
+	getline(cin,z);
+	cout<<"Hobi : ";
+	getline(cin,p);
+	cout<<endl;
+	cout<<"nama saya adalah "<<x<<endl<<"NPM saya "<<y<<endl<<"Alamat saya "<<z<<endl<<"hobi saya "<<p;
+	
+
+	
+	return 0;
+}
